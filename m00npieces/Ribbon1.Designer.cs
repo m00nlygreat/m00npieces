9bea987ab8bf34083d96a6827fda1a2014942092
m00nlygreat <m00nlygreat@gmail.com>
2019-04-08 18:01:27 +0900

    MatchSize기능 추가, Anchor 버튼 만듦.

--- m00npieces/Ribbon1.Designer.cs
+++ m00npieces/Ribbon1.Designer.cs
@@ -36,14 +36,27 @@
         {
             this.tab1 = this.Factory.CreateRibbonTab();
             this.group1 = this.Factory.CreateRibbonGroup();
+            this.group2 = this.Factory.CreateRibbonGroup();
             this.btnSwap = this.Factory.CreateRibbonButton();
+            this.btnMatchSize = this.Factory.CreateRibbonButton();
+            this.btnTL = this.Factory.CreateRibbonToggleButton();
+            this.btnML = this.Factory.CreateRibbonToggleButton();
+            this.btnBL = this.Factory.CreateRibbonToggleButton();
+            this.btnTC = this.Factory.CreateRibbonToggleButton();
+            this.btnMC = this.Factory.CreateRibbonToggleButton();
+            this.btnBC = this.Factory.CreateRibbonToggleButton();
+            this.btnTR = this.Factory.CreateRibbonToggleButton();
+            this.btnMR = this.Factory.CreateRibbonToggleButton();
+            this.btnBR = this.Factory.CreateRibbonToggleButton();
             this.tab1.SuspendLayout();
             this.group1.SuspendLayout();
+            this.group2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab1
             // 
             this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group2);
             this.tab1.Groups.Add(this.group1);
             this.tab1.Label = "m00npieces";
             this.tab1.Name = "tab1";
@@ -51,17 +64,87 @@
             // group1
             // 
             this.group1.Items.Add(this.btnSwap);
+            this.group1.Items.Add(this.btnMatchSize);
             this.group1.Label = "Swap";
             this.group1.Name = "group1";
             // 
+            // group2
+            // 
+            this.group2.Items.Add(this.btnTL);
+            this.group2.Items.Add(this.btnML);
+            this.group2.Items.Add(this.btnBL);
+            this.group2.Items.Add(this.btnTC);
+            this.group2.Items.Add(this.btnMC);
+            this.group2.Items.Add(this.btnBC);
+            this.group2.Items.Add(this.btnTR);
+            this.group2.Items.Add(this.btnMR);
+            this.group2.Items.Add(this.btnBR);
+            this.group2.Label = "Anchor";
+            this.group2.Name = "group2";
+            // 
             // btnSwap
             // 
+            this.btnSwap.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
             this.btnSwap.Image = global::m00npieces.Properties.Resources.swap__1_;
             this.btnSwap.Label = "교체";
             this.btnSwap.Name = "btnSwap";
             this.btnSwap.ShowImage = true;
             this.btnSwap.SuperTip = "2개를 선택해야 합니다.";
             this.btnSwap.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSwap_Clicked);
+            // 
+            // btnMatchSize
+            // 
+            this.btnMatchSize.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnMatchSize.Image = global::m00npieces.Properties.Resources.matchsize;
+            this.btnMatchSize.Label = "크기맞춤";
+            this.btnMatchSize.Name = "btnMatchSize";
+            this.btnMatchSize.ShowImage = true;
+            this.btnMatchSize.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BtnMatchSize_Click);
+            // 
+            // btnTL
+            // 
+            this.btnTL.Label = "○";
+            this.btnTL.Name = "btnTL";
+            // 
+            // btnML
+            // 
+            this.btnML.Label = "○";
+            this.btnML.Name = "btnML";
+            // 
+            // btnBL
+            // 
+            this.btnBL.Label = "○";
+            this.btnBL.Name = "btnBL";
+            // 
+            // btnTC
+            // 
+            this.btnTC.Label = "○";
+            this.btnTC.Name = "btnTC";
+            // 
+            // btnMC
+            // 
+            this.btnMC.Label = "○";
+            this.btnMC.Name = "btnMC";
+            // 
+            // btnBC
+            // 
+            this.btnBC.Label = "○";
+            this.btnBC.Name = "btnBC";
+            // 
+            // btnTR
+            // 
+            this.btnTR.Label = "○";
+            this.btnTR.Name = "btnTR";
+            // 
+            // btnMR
+            // 
+            this.btnMR.Label = "○";
+            this.btnMR.Name = "btnMR";
+            // 
+            // btnBR
+            // 
+            this.btnBR.Label = "○";
+            this.btnBR.Name = "btnBR";
             // 
             // Ribbon1
             // 
@@ -73,6 +156,8 @@
             this.tab1.PerformLayout();
             this.group1.ResumeLayout(false);
             this.group1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +167,17 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton btnSwap;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnMatchSize;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnTL;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnBL;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnTC;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnMC;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnBC;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnTR;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnMR;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnBR;
     }
 
     partial class ThisRibbonCollection
--- m00npieces/Ribbon1.cs
+++ m00npieces/Ribbon1.cs
@@ -39,19 +39,19 @@ namespace m00npieces
                 sel.ShapeRange[1].Left = floSecondLeft;
                 sel.ShapeRange[2].Top = floFirstTop;
                 sel.ShapeRange[2].Left = floFirstLeft;
-                if (intFirstZorder < intSecondZorder) // First냐, Second냐의 차이는 클릭 순서에 따라 달라진다. 암튼 뭐가 더 위에 있느냐에 따라,  
+                if (intFirstZorder < intSecondZorder) // First냐, Second냐의 차이는 클릭 순서에 따라 달라진다. 암튼 뭐가 더 위에 있느냐에 따라, 앞으로 보내거나 뒤로 보내야 하는 방향이 달라짐.
                 {
                     for (int i = intFirstZorder; i < intSecondZorder; i++)
                     {
                         sel.ShapeRange[1].ZOrder(MsoZOrderCmd.msoBringForward);
                     }
-                    for (int i = intSecondZorder - 1; i > intFirstZorder; i--)
+                    for (int i = intSecondZorder - 1; i > intFirstZorder; i--) // 두번째 도형의 순서를 옮길 때는, 한 번 덜 가야 한다. 왜냐면 첫번째 도형이 이미 두 번째 도형의 위치까지 와 있어서 서로 순서가 교체되었으므로.
                     {
                         sel.ShapeRange[2].ZOrder(MsoZOrderCmd.msoSendBackward);
                     }
 
                 }
-                else
+                else // 같은 내용을 방향 바꿔서 보냄.
                 {
                     for (int i = intFirstZorder; i > intSecondZorder; i--)
                     {
@@ -68,33 +68,12 @@ namespace m00npieces
                 return;
             }
 
-            //MessageBox.Show(sel.ShapeRange[2].Top.ToString() + " " + sel.ShapeRange[2].Left.ToString() + " " + sel.ShapeRange.Count.ToString());
-            //MessageBox.Show(sel.TextFrame.TextRange.Text);
-            //sel.TextFrame.TextRange.Text = strTag;
         }
-
-        //private void Button1_Click(object sender, RibbonControlEventArgs e)
-        //{
-        //    var sel = Globals.ThisAddIn.Application.ActiveWindow.Selection;
-        //    //MessageBox.Show(sel.ShapeRange.ZOrderPosition.ToString());
-        //    MessageBox.Show(WhereInSlide(sel.ShapeRange[1]).ToString());
-        //}
-
-        //private void Button3_Click(object sender, RibbonControlEventArgs e)
-        //{
-        //    var sel = Globals.ThisAddIn.Application.ActiveWindow.Selection;
-        //    WhereInSlide(sel.ShapeRange[1]);
-        //    //var curSlide = Globals.ThisAddIn.Application.ActiveWindow.View.Slide;
-        //    //// MessageBox.Show(curSlide.Shapes.Count.ToString());
-        //    //MessageBox.Show(curSlide.Shapes[1].ZOrderPosition.ToString());
-        //}
-        public int WhereInSlide(PowerPoint.Shape shape)
+        public int WhereInSlide(PowerPoint.Shape shape) //ZOrderPosition이 아닌, 실제 앞으로 보내기/뒤로 보내기 시 작동하는 선택 도형의 슬라이드 내 레이어 위치를 구한다.
         {
             int intOrderInSlide = 0;
             var curSlide = Globals.ThisAddIn.Application.ActiveWindow.View.Slide;
             var shapes = curSlide.Shapes;
-            //MessageBox.Show("슬라이드 내 도형 개수" + curSlide.Shapes.Count.ToString());
-            //MessageBox.Show("현재 도형의 ZOrder" + shape.ZOrderPosition.ToString());
 
             for (int i = 1; i <= shapes.Count; i++)
             {
@@ -105,6 +84,17 @@ namespace m00npieces
                 }
             }
             return intOrderInSlide;
+        }
+
+        private void BtnMatchSize_Click(object sender, RibbonControlEventArgs e) // 사이즈 매치
+        {
+            var sel = Globals.ThisAddIn.Application.ActiveWindow.Selection;
+
+            if (sel.ShapeRange.Count == 2) // 2개를 선택했을때만 작동.
+            {
+                sel.ShapeRange[2].Width = sel.ShapeRange[1].Width; // 이걸로 잘 될까 싶지만 놀랍도록 잘 된다.
+                sel.ShapeRange[2].Height = sel.ShapeRange[1].Height;
+            }
         }
     }
     //PowerPoint._Application myPPT = Globals.ThisAddIn.Application;
